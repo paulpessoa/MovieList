4bdb5cae99f615ce638016356e18619c47b623f2
Paul Pessoa <paulmspessoa@gmail.com>
2021-05-23 01:06:12 -0300

    atribuindo id para main, e mudando queryselectors para getElementById

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ const IMGPATH = 'https://image.tmdb.org/t/p/w1280';
 
 const SEARCHAPI = 'https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=';
 
-const main = document.querySelector('main');
-const form = document.querySelector('form');
+const main = document.getElementById('main');
+const form = document.getElementById('form');
 const search = document.getElementById('search');
 
 //inicialmente carrega filmes favoritos/populares
