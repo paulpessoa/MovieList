aedb694a346aa60e5a59427db88406076189f5a6
Paul Pessoa <paulmspessoa@gmail.com>
2021-05-23 01:04:56 -0300

    função search js

--- script.js
+++ script.js
@@ -3,42 +3,54 @@ const APIURL =
 
 const IMGPATH = 'https://image.tmdb.org/t/p/w1280';    
 
-const main = document.querySelector('main');
+const SEARCHAPI = 'https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=';
 
-    async function getMovies () {
-        const resp = await fetch (APIURL);
+const main = document.querySelector('main');
+const form = document.querySelector('form');
+const search = document.getElementById('search');
+
+//inicialmente carrega filmes favoritos/populares
+getMovies(APIURL);
+
+    async function getMovies (url) {
+        const resp = await fetch (url);
         const respData = await resp.json();
 
-        console.log(respData);
+//        console.log(respData);
+        showMovies(respData.results);
+//a função exibir movies foi criada inicialmente aqui.
 
-
-        respData.results.forEach(movie =>{
-            const { poster_path, title, vote_average} = movie;
-            
-            const movieEl = document.createElement('div');
-            movieEl.classList.add("movie");
-
-            movieEl.innerHTML =`
-            <img
-            src="${IMGPATH + poster_path}"
-            alt="${title}"
-            >
-            <div class="movie-info">
-                <h3>${title}</h3>
-                <span class="${getClassByRate
-                (vote_average)}">${vote_average}
-                </span>
-            </div>
-            
-            `;
-
-            main.appendChild(movieEl);
-
-
-        });
-
-        return respData;
+  //      return respData;
     }
+
+function showMovies (movies) {
+    //limpar margin
+    main.innerHTML ='';
+    movies.forEach(movie =>{
+        const { poster_path, title, vote_average} = movie;
+        
+        const movieEl = document.createElement('div');
+        movieEl.classList.add("movie");
+
+        movieEl.innerHTML =`
+        <img
+        src="${IMGPATH + poster_path}"
+        alt="${title}"
+        >
+        <div class="movie-info">
+            <h3>${title}</h3>
+            <span class="${getClassByRate
+            (vote_average)}">${vote_average}
+            </span>
+        </div>
+        
+        `;
+
+        main.appendChild(movieEl);
+    });
+
+}
+
 
 function getClassByRate (vote) {
     if (vote >= 8){
@@ -50,8 +62,17 @@ function getClassByRate (vote) {
     }
 };
 
-getMovies();
 
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const searchTerm = search.value;
+    
+    if (searchTerm) {
+        getMovies(SEARCHAPI + searchTerm);
+        search.value = '';
+    }
+});
 
 /*
 https://github.com/florinpop17/10-projects-10-hours/blob/master/movie-app/script.js
